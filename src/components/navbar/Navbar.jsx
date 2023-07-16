0185c93df3413d1010bfe97e6380c70d565cf186
Vageesh-tech <vageesh.cs20@rvce.edu.in>
2023-07-17 00:09:19 +0530

    front complete

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import "./navbar.css";
+import "./navbar.css"
 
-function Navbar(){
-    return (
-        <div className="navbar">
-            <div className="navContainer">
-                <span className="logo">Hotel Reservation</span>
-                <div className="navItems">
-                    <button className="navButton">Register</button>
-                    <button className="navButton">Login</button>
-                </div>
-            </div>
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="navContainer">
+        <span className="logo">Booking.com</span>
+        <div className="navItems">
+          <button className="navButton">Register</button>
+          <button className="navButton">Login</button>
+        </div>
       </div>
-    )
+    </div>
+  )
 }
-export default Navbar;
+
+export default Navbar
